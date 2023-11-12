4167af680fa64a94b78a9585031989593c0b470f
Sanoj <cksanoj@gmail.com>
2023-11-12 12:39:08 +0800

    Modify image urls and appearance

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 fetch('groceryItems.json')
-   .then(response => response.json())
-   .then(data => {
-       const container = document.getElementById('groceryItemsContainer');
-       data.forEach(item => {
-           const itemElement = document.createElement('div');
-           itemElement.className = 'thumbnail-grid bg-cover';
-           itemElement.innerHTML = `
-               <img src="${item.imageUrl}" alt="${item.itemName}">
-               <div class="item-name text-white bg-black">${item.itemName}</div>
-           `;
-           container.appendChild(itemElement);
-       });
-   });
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('groceryItemsContainer');
+        data.forEach(item => {
+            const itemElement = document.createElement('div');
+            itemElement.className = 'grid-item relative';
+            itemElement.innerHTML = `
+              <img class="w-full h-full object-cover" src="${item.imageUrl}" alt="${item.itemName}">
+              <div class="item-name absolute top-0 right-0 bg-black bg-opacity-50 text-white font-bold p-2">${item.itemName}</div>
+          `;
+            container.appendChild(itemElement);
+        });
+    });
